82c2247f50675ab2d1507521e89833c822728691
aymansalem22 <s.aymansalem@gmail.com>
2022-02-23 16:04:37 +0100

    second change

--- ShopmeEcommerce/ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
+++ ShopmeEcommerce/ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
@@ -3,6 +3,7 @@ package com.shopme.admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//ok
 @Controller
 public class MainController {
 
