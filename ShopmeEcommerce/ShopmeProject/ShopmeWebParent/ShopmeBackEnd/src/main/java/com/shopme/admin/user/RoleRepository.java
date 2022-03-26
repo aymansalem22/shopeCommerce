d005bc4a300c59a3c4bc9d425c2dddde0c6c7bb7
aymansalem22 <s.aymansalem@gmail.com>
2022-03-26 20:41:05 +0100

    update

--- ShopmeEcommerce/ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/RoleRepository.java
+++ ShopmeEcommerce/ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/RoleRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.shopme.common.entity.Role;
-
+//ok
 @Repository     
 public interface RoleRepository extends CrudRepository<Role,Integer> {
 
