3db863fd4328e66e864d2d7a5a73328e6275758e
aymansalem22 <s.aymansalem@gmail.com>
2022-04-21 06:46:31 +0200

    new update

---
+++ ShopmeEcommerce/ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/FileUploadUtil.java
@@ -0,0 +1,33 @@
+package com.shopme.admin;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileUploadUtil {
+
+	public static void saveFile(String uploadDir,String fileName,
+			MultipartFile multipartFile) throws IOException {
+		//first we need to get path and to create the directory with uploadDir
+		Path uploadPath=Paths.get(uploadDir);
+		//and if doesnt existe we create this directory 
+		if(!Files.exists(uploadPath)) {
+			Files.createDirectories(uploadPath);
+		}
+		//then we save the data in multipartFile object here as a file in the file system
+		try (InputStream inputStream=multipartFile.getInputStream()){
+			//we create the path for the file path..uploadPath
+			Path filePath=uploadPath.resolve(fileName);
+			Files.copy(inputStream, filePath,StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException ex) {
+			throw new IOException("Could not save file: "+fileName,ex);
+		}
+		
+	}
+
+}
--- ShopmeEcommerce/ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeEcommerce/ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -32,7 +32,7 @@ public class UserService {
 		return (List<Role>) roleRepo.findAll();
 	}
 
-	public void save(User user) {
+	public User save(User user) {
 		boolean isUpdatingUser=(user.getId()!=null);//this mean in updating mode
 		if(isUpdatingUser) {
 			User existingUser=userRepo.findById(user.getId()).get();
@@ -47,7 +47,7 @@ public class UserService {
 		}
 		
 		
-		userRepo.save(user);
+	return 	userRepo.save(user);
 		
 	}
 	
