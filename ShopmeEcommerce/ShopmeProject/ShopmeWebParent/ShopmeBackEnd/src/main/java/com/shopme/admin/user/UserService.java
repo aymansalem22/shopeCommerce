d2c264e032e21718568ddc2e2628f734ebd12125
aymansalem22 <s.aymansalem@gmail.com>
2023-01-31 13:44:04 +0100

    40. Code Sorting for Users List

--- ShopmeEcommerce/ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeEcommerce/ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -6,6 +6,7 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,10 +33,11 @@ public class UserService {
 		return (List<User>) userRepo.findAll();
 	}
 
-	public Page<User> listByPage(int pageNum) {
-		PageRequest pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE);
+	public Page<User> listByPage(int pageNum,String sortField,String sortDir) {
+		Sort sort=Sort.by(sortField);
+		sort=sortDir.equals("asc")?sort.ascending():sort.descending();
+		PageRequest pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE,sort);
 		return userRepo.findAll(pageable);
-
 	}
 
 	public List<Role> listRoles() {
